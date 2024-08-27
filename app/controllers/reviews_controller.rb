bb3b1640efc109c46f451930d4d7506674229a70
peyton owens <peyton21owens@gmail.com>
2024-08-27 18:45:14 -0400

    Added form for Reviews and create+new action within Reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,39 @@
 class ReviewsController < ApplicationController
 
+  before_action :set_movie
+
 def index
-  @movie = Movie.find(params[:movie_id])
   @reviews = @movie.reviews
 
 end
 
+def new
+  @review = @movie.reviews.new
+
+end
+
+def create
+  @review = @movie.reviews.new(review_params)
+
+  if @review.save
+    redirect_to movie_reviews_path(@movie),
+                                   notice: "Review Posted!"
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+
+
+  private
+    def review_params
+      params.require(:review).permit(:name, :stars, :comment)
+    end
+
+
+    private
+      def set_movie
+        @movie = Movie.find(params[:movie_id])
+      end
 
 end
