87b63e9ed07301c0718095274b273231c067c5e3
peyton owens <peyton21owens@gmail.com>
2024-09-16 18:25:51 -0400

    Added Users resource with password encryption

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, format:{ with: /\S+@\S+/},
+                    uniqueness: {case_sensitive: false }
+
+
+
+end
