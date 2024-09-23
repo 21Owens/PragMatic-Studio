c21d26ef9d19917565d7b0fa6a26913d9e88c287
peyton owens <peyton21owens@gmail.com>
2024-09-23 18:33:18 -0400

    Added sessions controller and a Sign In button

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+def new
+end
+
+def create
+  fail
+end
+
+def destroy
+end
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+resource :session, only: [:new,:create,:destroy]
+
+get "signin" => "sessions#new"
+
 resources :users
 
 get "signup" => "users#new"
