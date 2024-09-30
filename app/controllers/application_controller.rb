05b4b91d5f7d9aa944bf36dbccc00d361dc9b28c
21Owens <peyton21owens@gmail.com>
2024-09-30 17:21:13 -0400

    Added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
 
 add_flash_types(:danger)
 
+private 
+    def require_signin
+        unless current_user
+            redirect_to new_session_path, alert: 'Please sign in first!'
+        end
+    end
+
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end    
+
+helper_method :current_user
 end
