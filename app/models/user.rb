438ee9f680fdc862186aeb7026fb87a440c554fd
peyton owens <peyton21owens@gmail.com>
2024-10-15 20:32:34 -0400

    Added password character length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :favorite_movies, through: :favorites, source: :movie
-  
+
   has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, format:{ with: /\S+@\S+/},
                     uniqueness: { case_sensitive: false }
 
-  # validates :password, length: { minimum: 10 }
+  validates :password, length: { minimum: 10 }
 
 def gravatar_id
   Digest::MD5::hexdigest(email.downcase)
